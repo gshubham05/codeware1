c91221b2adc9a57cf385cacd2b623c57ec6cadc9
Shubham Goyal <111879491+gshubham05@users.noreply.github.com>
2026-01-18 22:26:52 +0530

    Fix import path for fbq in RouteChangeTracker

--- src/app/Components/RouteChangeTracker.jsx
+++ src/app/Components/RouteChangeTracker.jsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import * as fbq from "@/lib/fpixel";
+import * as fbq from "../lib/fpixel";
 
 export default function RouteChangeTracker() {
   const pathname = usePathname();
