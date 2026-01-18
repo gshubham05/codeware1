0ea7526280e26fc67f875e941f1a6cbcc077352f
Shubham Goyal <111879491+gshubham05@users.noreply.github.com>
2026-01-18 21:45:57 +0530

    Update Python institute name and add Meta Pixel code

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
     "best computer institute in Dehradun",
     "best python institute in Dehradun",
     "best computer institute in India",
-    "best python institute in India",
+    "best advance python institute in India",
     "best java institute in India",
     "best java institute in Dehradun",
     "Shubham Goyal",
@@ -167,6 +167,23 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+          <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '733095509850893');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=733095509850893&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
       </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
